334cf8076d4a9fa48efc94f2c5eed9c60578cdc0
jozinn <sikora10g@gmail.com>
2022-02-11 23:22:14 +0100

    Calculator finished

--- script.js
+++ script.js
@@ -29,22 +29,81 @@ function operate(a, b, operation) {
         case "/": result =  divide(a, b);
         break;
     }
+    if(result == "Pamietaj cholero nie dziel przez zero") return result;
+    result = result.toString();
+    if(result.length > 10) {
+        result = +result;
+        result = result.toPrecision(10);
+    };
     return result;
 }
-
+function equals(num1, num2, op) {
+    if(num1 == null || op == null) return;
+    num2 = +display.textContent;
+    display.textContent = operate(num1, num2, op);
+    a = null;
+    b = null;
+    operation = null;
+    document.querySelector(".clicked").classList.remove("clicked");
+}
 const keypad = document.getElementById("keypad");
 const display = document.getElementById("display");
 const clear = document.getElementById("clear");
 const negation = document.getElementById("negation");
 const percent = document.getElementById("percent");
+const division = document.getElementById("division");
+const multiplication = document.getElementById("multiplication");
+const substraction = document.getElementById("substraction");
+const addition = document.getElementById("addition");
+const equal = document.getElementById("equal");
+let operation;
+let a;
+let b;
 keypad.addEventListener("click", e => {
     let target = e.target;
     if(target.id == "clear") return;
     if(target.id == "negation") return;
     if(target.id == "percent") return;
+    if(target.id == "division") return;
+    if(target.id == "multiplication") return;
+    if(target.id == "substraction") return;
+    if(target.id == "addition") return;
+    if(target.id == "equal") return;
+    if(operation != null) display.textContent = "";
     if(display.textContent === "0") display.textContent = "";
     display.textContent += target.textContent;
 })
-clear.addEventListener("click", () => display.textContent = "0");
+clear.addEventListener("click", () => {
+    a = null;
+    b = null;
+    operation = null;
+    if(document.querySelector(".clicked")) document.querySelector(".clicked").classList.remove("clicked");
+    display.textContent = "0";
+});
 negation.addEventListener("click", () => display.textContent = -(+display.textContent));
 percent.addEventListener("click", () => display.textContent = +display.textContent / 100);
+division.addEventListener("click", () => {
+    if(operation != null) equals(a, b, operation);
+    division.classList.add("clicked");
+    a = +display.textContent;
+    operation = "/";
+});
+multiplication.addEventListener("click", () => {
+    if(operation != null) equals(a, b, operation);
+    multiplication.classList.add("clicked");
+    a = +display.textContent;
+    operation = "*";
+});
+substraction.addEventListener("click", () => {
+    if(operation != null) equals(a, b, operation);
+    substraction.classList.add("clicked");
+    a = +display.textContent;
+    operation = "-";
+});
+addition.addEventListener("click", () => {
+    if(operation != null) equals(a, b, operation);
+    addition.classList.add("clicked");
+    a = +display.textContent;
+    operation = "+";
+});
+equal.addEventListener("click", () => equals(a, b, operation));
